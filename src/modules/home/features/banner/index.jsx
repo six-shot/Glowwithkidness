134606becfc72af731ba86c8d143364923b2ba01
six-shot <okhuomonajayi54@gmail.com>
2024-07-23 17:53:56 +0000

    fix

--- src/modules/home/features/banner/index.jsx
+++ src/modules/home/features/banner/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Banner() {
   return (
-    <div className="grid sm:grid-cols-3 grid-cols-1 gap-10 px-[5%] mt-[500px] ">
+    <div className="grid sm:grid-cols-3 grid-cols-1 sm:gap-10 gap-6 px-[5%] mt-[500px] ">
       <div className="col-span-1">
         <div className="bg-[#F1CEDD] py-[12%] w-full px-[10%] flex justify-center items-center flex-col">
           <h5 className="uppercase">Book appointment</h5>
@@ -10,7 +10,7 @@ export default function Banner() {
             You may check our gallery for great styles or click to schedule an
             appointment with us
           </p>
-          <button className="sm:flex hidden bg-black text-sm rounded-[8px] text-white sm:px-5 px-2 py-2.5 mt-5">
+          <button className="bg-black text-sm rounded-[8px] text-white sm:px-5 px-2 py-2.5 mt-5">
             Book Now
           </button>
         </div>
@@ -22,7 +22,7 @@ export default function Banner() {
             You may check our gallery for great styles or click to schedule an
             appointment with us
           </p>
-          <button className="sm:flex hidden bg-black text-sm rounded-[8px] text-white sm:px-5 px-2 py-2.5 mt-5">
+          <button className=" bg-black text-sm rounded-[8px] text-white sm:px-5 px-2 py-2.5 mt-5">
             Book Now
           </button>
         </div>
@@ -34,7 +34,7 @@ export default function Banner() {
             You may check our gallery for great styles or click to schedule an
             appointment with us
           </p>
-          <button className="sm:flex hidden bg-black text-sm rounded-[8px] text-white sm:px-5 px-2 py-2.5 mt-5">
+          <button className=" bg-black text-sm rounded-[8px] text-white sm:px-5 px-2 py-2.5 mt-5">
             Book Now
           </button>
         </div>
