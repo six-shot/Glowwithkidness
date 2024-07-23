4c1c533d6f32223a1cea54e23bd8748317dc454b
six-shot <okhuomonajayi54@gmail.com>
2024-07-23 19:22:01 +0000

    fix

--- src/modules/home/features/banner/index.jsx
+++ src/modules/home/features/banner/index.jsx
@@ -1,44 +1,52 @@
-import React from 'react'
+import React from "react";
 
 export default function Banner() {
   return (
-    <div className="grid sm:grid-cols-3 grid-cols-1 sm:gap-10 gap-6 px-[5%] mt-[500px] ">
-      {/* <div className="col-span-1">
-        <div className="bg-[#F1CEDD] py-[12%] w-full px-[10%] flex justify-center items-center flex-col">
-          <h5 className="uppercase">Book appointment</h5>
-          <p className="py-[2%]">
-            You may check our gallery for great styles or click to schedule an
-            appointment with us
-          </p>
-          <button className="bg-black text-sm rounded-[8px] text-white sm:px-5 px-2 py-2.5 mt-5">
-            Book Now
-          </button>
+    <div className="sm:mt-[520px] mt-[500px] px-[5%] mb-[2%]">
+      <h5 className="text-black tracking-[6px] text-lg">
+        MOST POPULAR SERVICES
+      </h5>
+      <div className="grid sm:grid-cols-4 grid-cols-1 sm:gap-10  ">
+        <div className="col-span-1">
+          <div className="w-full sm:mt-[15%] mt-[4%]">
+            <h6 className="text-sm">01</h6>
+            <div className="border-b border-[#00000073] mt-5"></div>
+            <h6 className="sm:mt-5 mt-2 sm:text-[45px] text-[30px] font-medium hover:italic cursor-pointer">
+              Clipping
+            </h6>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="w-full sm:mt-[15%] mt-[4%]">
+            <h6 className="text-sm">02</h6>
+            <div className="border-b border-[#00000073] mt-5"></div>
+            <h6 className="sm:mt-5 mt-2 sm:text-[45px] text-[30px] font-medium hover:italic cursor-pointer">
+              Stylization
+            </h6>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="w-full sm:mt-[15%] mt-[4%]">
+            <h6 className="text-sm">03</h6>
+            <div className="border-b border-[#00000073] mt-5"></div>
+            <h6 className="sm:mt-5 mt-2 sm:text-[45px] text-[30px] font-medium hover:italic cursor-pointer">
+              Colorization
+            </h6>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="w-full sm:mt-[15%] mt-[4%]">
+            <h6 className="text-sm">04</h6>
+            <div className="border-b border-[#00000073] mt-5"></div>
+            <h6 className="sm:mt-5 mt-2 sm:text-[45px] text-[30px] font-medium hover:italic cursor-pointer">
+              Care
+            </h6>
+          </div>
         </div>
       </div>
-      <div className="col-span-1">
-        <div className="bg-[#F1CEDD] py-[12%] w-full px-[10%] flex justify-center items-center flex-col">
-          <h5 className="uppercase">Book appointment</h5>
-          <p className="py-[2%]">
-            You may check our gallery for great styles or click to schedule an
-            appointment with us
-          </p>
-          <button className=" bg-black text-sm rounded-[8px] text-white sm:px-5 px-2 py-2.5 mt-5">
-            Book Now
-          </button>
-        </div>
-      </div>
-      <div className="col-span-1">
-        <div className="bg-[#F1CEDD] py-[12%] w-full px-[10%] flex justify-center items-center flex-col">
-          <h5 className="uppercase">Book appointment</h5>
-          <p className="py-[2%]">
-            You may check our gallery for great styles or click to schedule an
-            appointment with us
-          </p>
-          <button className=" bg-black text-sm rounded-[8px] text-white sm:px-5 px-2 py-2.5 mt-5">
-            Book Now
-          </button>
-        </div>
-      </div> */}
+      <button className="mt-4 sm:mt-8 bg-black text-sm rounded-[8px] text-white sm:px-8 px-4 sm:py-4 py-2.5">
+        See Pricelist
+      </button>
     </div>
   );
 }
